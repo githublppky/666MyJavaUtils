509f59a9bf7ccacb84f800cf364f86abbc7de341
githublppky <319759848@qq.com>
2019-11-06 21:15:09 +0800

    封装了一些常用Java操作方法,便于重复开发利用.liupeng

---
+++ src/test/java/com/opslab/util/algorithmImpl/FileTypeImplTest.java
@@ -0,0 +1,18 @@
+package com.opslab.util.algorithmImpl;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import com.opslab.util.TestUtil;
+
+import java.io.File;
+
+public class FileTypeImplTest extends TestCase {
+
+    @Test
+    public void testFileType() {
+        assertEquals("gif", FileTypeImpl.getFileType(new File(TestUtil.path + "/image/ali.gif")));
+        assertEquals("png", FileTypeImpl.getFileType(new File(TestUtil.path + "/image/tgepng")));
+        System.out.println(FileTypeImpl.getFileType(new File(TestUtil.path + "/image/ali.gif")));
+    }
+
+}
